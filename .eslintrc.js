f5a21788be243ca0321d1c174a5d29ed0b1b3189
Martin Zlámal <mrtnzlml@gmail.com>
2021-08-06 09:01:33 -0500

    Abacus: use the new Next.js eslint preset
    adeira-source-id: 494c18cb99d977d8f8cf6849a4339226705134a4

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,22 @@
 // @flow strict
 
+/* eslint-disable no-unused-vars */
+const OFF = 0;
+const WARN = 1;
+const ERROR = 2;
+/* eslint-enable no-unused-vars */
+
 module.exports = {
   env: {
     browser: true,
   },
+  extends: [
+    '@adeira/eslint-config/strict', // preset with almost everything
+    '@adeira/eslint-config/next', // additional Next.js rules
+  ],
   rules: {
     'react/forbid-elements': [
-      'error',
+      ERROR,
       {
         forbid: [{ element: 'input', message: 'use <Input> or <InputSubmit> instead' }],
       },
